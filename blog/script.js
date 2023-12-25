afa7c8d2f02d088b9b9131ffa05b3e051be33166
samidehil16 <dehil.sami16@gmail.com>
2023-12-25 02:56:14 +0100

    add(script): script service v3.0

--- blog/script.js
+++ blog/script.js
@@ -1,35 +1,15 @@
 const Docker = require('dockerode');
-const { error } = require('node:console');
 const { exit } = require('node:process');
 const docker = new Docker();
+const path = require('path');
 const readline = require('node:readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// readline.question("What's your name?", name => {
-//   console.log('name who is', name);
-//   readline.close();
-// });
 
 
-async function createBuilImage(__dirname,imageName){
-  try{
-    let stream = await docker.buildImage({
-      context: __dirname,
-      src: ['Dockerfile','file1','file2']
-    }, {t: imageName}, function (err, response) {
-      //console.log(response)
-    });
 
-    await new Promise((resolve, reject) => {
-      dockerode.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
-    });
-
-  }catch(err) {
-    console.log(err);
-  }
-}
 
 // Fonction pour démarrer un service Docker
 async function startDockerService(imageName, containerName, startCommand) {
@@ -50,16 +30,53 @@ async function startDockerService(imageName, containerName, startCommand) {
 
 
 
+
+
+
+
+// Construction de l'image à partir du répertoire contextuel
+
+ function buildImage(contextPath ,imageName){
+
+   docker.buildImage({ context: contextPath, src: ['Dockerfile'], t: imageName }, (error, stream) => {
+      if (error) {
+          console.error('Erreur lors de la construction de l\'image :', error);
+          return;
+      }
+
+      // Affichage des logs de construction de l'image
+      docker.modem.followProgress(stream, onFinished);
+
+      function onFinished(err, output) {
+          if (err) {
+              console.error('Erreur lors de la construction de l\'image :', err);
+          } else {
+              console.log('Image construite avec succès :', output);
+          }
+      }
+  });
+
+}
+
+
+
 // Fonction pour tester un service Docker
-function testServiceDocker(container) {
-  console.log(`Test du service Docker ${container.id}.`);
-  console.log(`État du service ${container.id} : ${container.state}`);
+function testServiceDocker(idContainer) {
+  const res = docker.getContainer(idContainer)
+  let state = '';
+  const info = res.inspect((err,data)=>{
+    if(err){
+      console.log(err)
+    } else state = data.State.Status
+  })
+  console.log(`Test du service Docker ${res.id}.`);
+  console.log(`État du service ${res.id} : ${state}`);
 }
 
 // Exemple : Déploiement et test d'un microservice
 async function deployAndTestMicroservice() {
-  const serviceName = 'client'; // Nom de votre service
-  const imageName = 'sami/client:latest'; // Nom de l'image Docker
+  const serviceName = ""; // Nom de votre service
+  const imageName = ""; // Nom de l'image Docker
   const startCommand = 'npm start'; // Commande pour démarrer votre service
 
   console.log(`Déploiement de ${serviceName}...`);
@@ -79,11 +96,20 @@ async function deployAndTestMicroservice() {
 // Appel de la fonction pour déployer et tester un microservice
  //deployAndTestMicroservice();
 
-// Fonction pour arrêter un service Docker
+// Fonction pour supprimer un service Docker
 async function RemoveDockerService(container) {
   try {
-    await container.stop();
-    await container.remove();
+    await container.stop(); // fonction de dockerode qui stop un container
+    await container.remove(); // fonction de dockerode qui supprime un container
+    console.log(`Service Docker ${container.id} arrêté et supprimé.`);
+  } catch (err) {
+    console.error('Erreur lors de l\'arrêt du service Docker :', err);
+  }
+}
+
+async function stopDockerService(container) {
+  try {
+    await container.stop(); // fonction de dockerode qui stop un container
     console.log(`Service Docker ${container.id} arrêté et supprimé.`);
   } catch (err) {
     console.error('Erreur lors de l\'arrêt du service Docker :', err);
@@ -93,17 +119,19 @@ async function RemoveDockerService(container) {
 
  function automatisation(){
 
-  readline.question(' What do you want? \n', expr =>{
+  readline.question(' What do you want? \n', expr =>{ // permet de crée une intéraction dans un terminal et de récupérer la réponse
     switch(expr) {
 
       case'create-build-image':
-            let imageName ='';
-            let directory ='';
+            
             readline.question('Take me name of Docker image \n', name =>{
-              imageName = name 
+              const options = {
+                t: name // Nom et tag de l'image à construire
+            };
               readline.question('What is your path to the DockerFile ? \n', dirname =>{
-                directory = dirname
-                createBuilImage(directory,imageName);
+                const contextPath = path.resolve(__dirname, dirname);
+                  console.log(options.t);
+                  buildImage(contextPath,name);
               })
             });
         break;
@@ -135,13 +163,12 @@ async function RemoveDockerService(container) {
             readline.question('Number of container please ? \n',answer =>{
               var container = docker.getContainer(answer); // récuparation du container par ID
               if (container) {
-                RemoveDockerService(container);
+                RemoveDockerService(container); // permet de supprimer un container
               } else console.log("Ce container existe pas") 
                       
             });
             
       break;
-
       
       case'info-container-service':
         readline.question('Number of container please ? \n',answer =>{
@@ -155,7 +182,7 @@ async function RemoveDockerService(container) {
       break;
       
       case'exit':
-            exit();
+            exit(); // Permet de quitter le srcipt
       break;
 
       default: 
@@ -169,4 +196,4 @@ async function RemoveDockerService(container) {
 
  }
 
-automatisation();
+ automatisation();
